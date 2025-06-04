db0c9a047861ee81466339ca0fbece753d19293f
Joe Dee <joseph.dee@gmail.com>
2025-06-04 09:35:12 -0400

    Scaling down
    Scaling down

--- src/components/Scene/Model.js
+++ src/components/Scene/Model.js
@@ -22,7 +22,7 @@ export default function Model() {
     })
     
     return (
-        <group scale={viewport.width / 3.75} >
+        <group scale={viewport.width / 7.5} >
             <Text font={'/fonts/PPNeueMontreal-Bold.otf'} position={[0, 0, -1]} fontSize={0.5} color="white" anchorX="center" anchorY="middle">
                 hello world!
             </Text>
