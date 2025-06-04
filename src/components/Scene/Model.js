3524065865e296ecad14c9634c6080858f82ef61
Joe Dee <joseph.dee@gmail.com>
2025-06-04 11:29:38 -0400

    Why did it turn black?
    Why did it turn black?

--- src/components/Scene/Model.js
+++ src/components/Scene/Model.js
@@ -10,23 +10,21 @@ export default function Model() {
     
     useFrame( () => {
         torus.current.rotation.z += 0.02
-        torus.current.rotation.x += 0.02
-        torus.current.rotation.y += 0.02
     })
 
     const materialProps = useControls({
-        thickness: { value: 0.15, min: 0, max: 3, step: 0.05 },
+        thickness: { value: 0.2, min: 0, max: 3, step: 0.05 },
         roughness: { value: 0, min: 0, max: 1, step: 0.1 },
         transmission: {value: 1, min: 0, max: 1, step: 0.1},
         ior: { value: 1.2, min: 0, max: 3, step: 0.1 },
-        chromaticAberration: { value: 0.00, min: 0, max: 1},
+        chromaticAberration: { value: 0.02, min: 0, max: 1},
         backside: { value: true},
     })
     
     return (
         <group scale={viewport.width / 20} >
-            <Text font={'/fonts/Merriweather.ttf'} position={[0, 0, -1]} fontSize={1.5} color="black" anchorX="center" anchorY="middle">
-                It&apos;s time to see health differently.
+            <Text font={'/fonts/Merriweather.ttf'} position={[0, 0, -1]} fontSize={1.5} color="white" anchorX="center" anchorY="middle">
+                It&apos;s time to d
             </Text>
             <mesh ref={torus} {...nodes.Torus002}>
                 <MeshTransmissionMaterial {...materialProps}/>
